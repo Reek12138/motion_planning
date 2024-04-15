eefb4f031ab373917b9eda22fb30f547ee458515
reek <2894487027@qq.com>
2024-04-15 18:20:50 +0800

    bivp

---
+++ L5/minimum_jerk_BIVP/build/lec5_hw/CMakeFiles/click_gen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/reek/motion_planning/L5/minimum_jerk_BIVP/devel/lib/lec5_hw/click_gen"
+  "/home/reek/motion_planning/L5/minimum_jerk_BIVP/devel/lib/lec5_hw/click_gen.pdb"
+  "CMakeFiles/click_gen.dir/src/click_gen.cpp.o"
+  "CMakeFiles/click_gen.dir/src/click_gen.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/click_gen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ L5/minimum_jerk_BIVP/build/lec5_hw/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/reek/motion_planning/L5/minimum_jerk_BIVP/src/lec5_hw
+# Build directory: /home/reek/motion_planning/L5/minimum_jerk_BIVP/build/lec5_hw
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
